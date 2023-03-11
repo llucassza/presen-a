2645866901213bbe67d6b09ffc8e6727ed5af31f
Lucas <lucasdsouza15@gmail.com>
2023-03-11 16:21:26 -0300

    ssss

--- menu.py
+++ menu.py
@@ -1,30 +1,35 @@
 from controller import salvar, listar, presenca, salvarpresenca
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
 
 def menu():
     menu = 1
 
     while (menu != 0):
 
-        menu = int(input("1 - Fazer cadastro\n2 - Registrar presença\n3 - CRIAR LISTAR \n0 - Sair do sistema\n "))
+        menu = int(input("1 - Fazer cadastro\n2 - Registrar presença\n3 - Gerar listas \n0 - Sair do sistema\n "))
 
         match menu:
 
             case 1:
                             pessoa = {}
-                            pessoa['nome'] = input("Digite seu nome: ")
-                            pessoa['cpf'] = input("Digite seu cpf: ")
-                            pessoa['telefone'] = input("digite seu telefone")
-                            pessoa['senha'] = int(input("Digite sua senha: "))
+                            pessoa['Nome'] = input("Digite seu nome: ")
+                            pessoa['CPF'] = input("Digite seu cpf: ")
+                            pessoa['Telefone'] = input("digite seu telefone: ")
+                            pessoa['Senha'] = int(input("Digite sua senha: "))
 
                             salvar(pessoa)
 
             case 2:
                 registro = {}
-                registro['nomes'] = input("Digite seu nome: ")
-                registro['senhas'] = input("Digite sua senha: ")                           
-                registro['horas'] = input('Digite que horário você chegou na aula: ')
+                registro['CPF'] = input("Digite seu CPF: ")                          
+                registro['Horário de registro'] = (current_time)
 
                 salvarpresenca(registro)
+                print(f"Presença Registrada ás {current_time}")
 
             case 3:
                 listar()
